357084c7b4f1f4b01618e322f28f61b3a5fafcc8
aadiuppal <aadiuppal@gmail.com>
2014-10-22 13:28:45 -0500

    kapo's q1

---
+++ q1.py
@@ -0,0 +1,10 @@
+def abbreviate_full_name(name):
+	#    name=raw_input("")
+	    list_name=list(name)
+	    length=len(list_name)
+	    i=length-1
+	    while(list_name[i]!= " "):
+	        i=i-1
+	    count=i
+	    i=i+1
+	  last_list=[]
