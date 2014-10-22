41c6c98c0361b5a81ddcfc45491b60e3000a8125
aadiuppal <aadiuppal@gmail.com>
2014-10-22 16:34:36 -0500

    q1 kapo complete

--- q1.py
+++ q1.py
@@ -8,3 +8,18 @@ def abbreviate_full_name(name):
 	    count=i
 	    i=i+1
 	  last_list=[]
+ for x in range(i, length):
+	        last_list.append(list_name[x])
+	    last_name=''.join(last_list)
+	    i=0
+	    ab_name=[]
+	    for x in range(i, count):
+	        if x==0:
+	            ab_name.append(list_name[x])
+	        elif list_name[x]==" ": 
+	            ab_name.append(list_name[x+1])
+	    ab=". ".join(ab_name)
+	    return ab + '. '+last_name
+if __name__=='__main__':
+	    for name in ['John Smith','Anna Maria Simpson','Bob Alan Faria Stewart']:
+	        print abbreviate_full_name(name)
